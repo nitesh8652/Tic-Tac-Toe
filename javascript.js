5274059e786c48921bc410c2b5a00a8d3ffcb4e3
Nitesh Salian <niteshsalian8652085233@gmail.com>
2024-07-24 21:23:41 +0530

    added patterns

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 
-
+let turnO = true;
 const patterns = [
     [0, 1, 2],
     [0, 3, 6],
@@ -16,12 +16,32 @@ const patterns = [
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("clicked");
-        box.innerText="x";
+        if (turnO) {
+            box.innerText = "X";
+            turnO = false;
+        } else {
+            box.innerText = "O";
+            turnO = true;
+        }
+        box.disabled = true;
+        winner();
+
     });
 });
-console.log(box)
+const winner = () => {
+    for (let wins of patterns) {
+        console.log(boxes[patterns[0]], boxes[patterns[1]], boxes[patterns[2]]);
+        let position0 = boxes[wins[0]].innerText;
+        let position1 = boxes[wins[1]].innerText;
+        let position2 = boxes[wins[2]].innerText;
 
-
+        if (position0 != "" && position1 != "" && position2 !== "") {
+            if (position0 == position1 && position1 == position2) {
+                console.log("Winnrrrr", position0);
+            }
+        }
+    }
+}
 
 
 
